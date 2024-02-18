ad864ef83ee80d42d5795384cf531c3ae66a9b98
Caio Madeira <caiomadeiracontato@outlook.com>
2024-02-17 21:03:46 -0300

    some tests with reference API

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
-print("test")
+frame = 0
+
+function init() -- CALLED WHEN THE SCRIPT INITIALIZES
+    DebugPrint("init main.lua")
+end
+
+function tickFrame()
+    frame = frame + 1
+    DebugPrint("Frame: ".. frame)
+end
