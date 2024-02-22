35e417bcfa60bcbd4bee8818f572ee1a7e56fd2a
Caio Madeira <caiomadeiracontato@outlook.com>
2024-02-22 02:23:31 -0300

    add skybox from another mod just for test and test some api features

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scidown
 A teardown sci-fi mod.
+
+# SKYBOX
+
+place in your mods folder and then in enviroment node (in editor) type: skybox=MOD/my_skybox.dds
--- main.lua
+++ main.lua
@@ -1,10 +1,32 @@
 frame = 0
 
 function init() -- CALLED WHEN THE SCRIPT INITIALIZES
+    local Player = {
+        pos = GetPlayerPos()
+    }
     DebugPrint("init main.lua")
 end
 
-function tickFrame()
+function tick()
+    -- DebugPrint("Player Velocity: "..GetPlayerVelocity())
     frame = frame + 1
-    DebugPrint("Frame: ".. frame)
+    defaultSpaceGravity(frame)
 end
+
+function defaultSpaceGravity(frame)
+    SetPlayerVelocity(VecAdd(GetPlayerVelocity(), Vec(0, 50*frame, 0)))
+end
+
+
+
+--[[ Gravity function - back another time to this
+function update(dt)
+    bodies = FindBodies(nil,true)
+    SetPlayerVelocity(VecAdd(GetPlayerVelocity(),Vec(0,dt*10,0)))
+    for i in pairs(bodies) do
+        local btrans = GetBodyTransform(bodies[i])
+        if IsBodyActive(bodies[i]) then
+            ApplyBodyImpulse(bodies[i],TransformToParentPoint(btrans,GetBodyCenterOfMass(bodies[i])),Vec(0,GetBodyMass(bodies[i])*dt*10,0))
+        end
+    end
+end ]]--
